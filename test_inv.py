936a088d029e2519e33f14db787431450dbd1b77
alirobi <alirobi@umich.edu>
2019-11-07 16:06:00 -0500

    changed to 1 arguement

--- test_inv.py
+++ test_inv.py
@@ -9,5 +9,5 @@ class TestBasics(unittest.TestCase):
 
 class TestBasics(unittest.TestCase):
     def test_mean(self):
-        result = inv.calculate("2 2 -")
+        result = inv.calculate("2 -")
         self.assertEqual(2, result)
