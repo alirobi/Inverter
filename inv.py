caf3a1daea884ae54f3cd79a7bbcf1812de2276e
alirobi <alirobi@umich.edu>
2019-11-07 16:29:00 -0500

    put add back

--- inv.py
+++ inv.py
@@ -5,7 +5,7 @@ import math
 
 
 operators = {
-    # '+': operator.add,
+    '-': operator.add,
     '+': math.log,
 }
 
--- test_inv.py
+++ test_inv.py
@@ -2,10 +2,10 @@ import unittest
 
 import inv
 
-# class TestBasics(unittest.TestCase):
-#     def test_add(self):
-#         result = inv.calculate("1 1 +")
-#         self.assertEqual(2, result)
+class TestBasics(unittest.TestCase):
+    def test_add(self):
+        result = inv.calculate("1 1 -")
+        self.assertEqual(2, result)
 
 class TestBasics(unittest.TestCase):
     def test_log(self):
